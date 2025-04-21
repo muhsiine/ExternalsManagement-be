dabf1e0993c4084744d8dfdbbe74ce13901762f3
muhsiine <muhsiine@gmail.com>
2025-04-21 19:13:49 +0100

    Remove unused Lombok annotations from ErrorResponse class
    Eliminated @Getter and @Setter annotations as they were unused. This reduces unnecessary dependencies and keeps the class cleaner and more maintainable. No functional changes have been made.

--- src/main/java/ma/nttdata/externals/commons/exception/ErrorResponse.java
+++ src/main/java/ma/nttdata/externals/commons/exception/ErrorResponse.java
@@ -1,8 +1,6 @@
 package ma.nttdata.externals.commons.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
