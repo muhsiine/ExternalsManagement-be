d3f97aa9ed5b1f834ed13b7b89c9c7b2938dfdd6
Aya Rouah <ayarouah0@gmail.com>
2025-06-02 16:36:23 +0100

    service et controller de interview

--- src/test/java/ma/nttdata/externals/ExternalsManagementBeApplicationTests.java
+++ src/test/java/ma/nttdata/externals/ExternalsManagementBeApplicationTests.java
@@ -6,16 +6,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import ma.nttdata.externals.config.TestConfig;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-    "spring.docker.compose.enabled=false",
-    "app.mock.flag=true",
-    "app.ai.server.url=http://mock-ai-server"
-})
-@ContextConfiguration(classes = TestConfig.class)
+@Import(TestConfig.class)
 class ExternalsManagementBeApplicationTests {
 
 	@Test
