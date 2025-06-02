5258f45ccb3fc18c3ca47a48462dde3252424ab3
Aya Rouah <ayarouah0@gmail.com>
2025-06-02 16:24:00 +0100

    service et controller de interview

--- src/test/java/ma/nttdata/externals/ExternalsManagementBeApplicationTests.java
+++ src/test/java/ma/nttdata/externals/ExternalsManagementBeApplicationTests.java
@@ -2,12 +2,20 @@ package ma.nttdata.externals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.docker.compose.enabled=false",
+    "app.mock.flag=true"
+})
 class ExternalsManagementBeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test will verify that the Spring application context loads successfully
 	}
 
 }
