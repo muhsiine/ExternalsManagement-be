2f561f99b255e824b8e373d438a0cee1a3b9ce1e
kanbouh zouhair <kanbouh.zuhair@gmail.com>
2025-07-20 20:41:56 +0100

    resolve test unit issue in offer

--- src/test/java/ma/nttdata/externals/module/offer/service/impl/OfferSrvImplTest.java
+++ src/test/java/ma/nttdata/externals/module/offer/service/impl/OfferSrvImplTest.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class OfferServImplTest {
+class OfferSrvImplTest {
 
     @Mock
     private OfferRepository offerRepository;
