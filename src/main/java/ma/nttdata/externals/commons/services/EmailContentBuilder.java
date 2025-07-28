27d9bbb19eedddcee680a8dfcdf6f740e086dc42
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-07-28 12:53:18 +0100

    moved the plain html text in emailcontenBuilder to template folder inside resources

--- src/main/java/ma/nttdata/externals/commons/services/EmailContentBuilder.java
+++ src/main/java/ma/nttdata/externals/commons/services/EmailContentBuilder.java
@@ -1,23 +1,25 @@
 package ma.nttdata.externals.commons.services;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 
 public class EmailContentBuilder {
 
     public static String buildInterviewEmail(String fullName, String offerTitle, String link, String scheduledDate) {
-        return """
-                <div style="max-width:600px;margin:0 auto;padding:20px;font-family:sans-serif;border:1px solid #ccc;border-radius:8px;">
-                    <h2 style="color:#004c97;">NTT DATA Interview</h2>
-                    <hr style="border:0;height:1px;background:#ccc;margin:10px 0;">
-                    <p>Hello <strong>%s</strong>,</p>
-                    <p>You are invited for an interview for the position of <strong>%s</strong>.</p>
-                    <p><strong>Date:</strong> %s</p>
-                    <p><strong>Link:</strong> <a href="%s" target="_blank">%s</a></p>
-                    <br>
-                    <p style="color:#666;font-size:12px;">
-                        NTT DATA<br>
-                        Parc Tétouan Shore, Shore 3, Route de Cabo Negro, Martil-Maroc.
-                    </p>
-                </div>
-                """.formatted(fullName, offerTitle, scheduledDate, link,link);
+        try{
+            String interviewInvitationTemplate = loadTemplate("templates/interviewsInvitation.html");
+            return  interviewInvitationTemplate.formatted(fullName,offerTitle,scheduledDate,link,link);
+        }catch (IOException e){
+            throw new RuntimeException("Failed to load email template", e);
+        }
+    }
+
+    private static String loadTemplate(String templatePath) throws IOException {
+        ClassPathResource resource = new ClassPathResource(templatePath);
+        return new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
     }
 }
