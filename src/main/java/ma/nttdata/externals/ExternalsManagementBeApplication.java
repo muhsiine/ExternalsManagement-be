77542d1d5ebfc2139edcf84658e30ec5af9f55c3
Ismail-Ah <ahakayismail4@gmail.com>
2025-08-12 12:51:33 +0100

    feat(env): move mail config from yml file to environment variables

--- src/main/java/ma/nttdata/externals/ExternalsManagementBeApplication.java
+++ src/main/java/ma/nttdata/externals/ExternalsManagementBeApplication.java
@@ -3,10 +3,17 @@ package ma.nttdata.externals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ExternalsManagementBeApplication {
 
+
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(ExternalsManagementBeApplication.class, args);
 	}
 
