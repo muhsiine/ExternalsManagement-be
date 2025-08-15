99b19a816279ba134e1cd8adb2dd37b08b8078fe
Ismail-Ah <ahakayismail4@gmail.com>
2025-08-15 11:45:25 +0100

    Revert "feat(env): move mail config from yml file to environment variables"
    This reverts commit 77542d1d5ebfc2139edcf84658e30ec5af9f55c3.

--- src/main/java/ma/nttdata/externals/ExternalsManagementBeApplication.java
+++ src/main/java/ma/nttdata/externals/ExternalsManagementBeApplication.java
@@ -3,17 +3,10 @@ package ma.nttdata.externals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ExternalsManagementBeApplication {
 
-
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
 		SpringApplication.run(ExternalsManagementBeApplication.class, args);
 	}
 
