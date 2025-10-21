77c530f4da0e38cb432ff1cd8fc7dc9df4fd92f9
Ismail-Ah <ahakayismail4@gmail.com>
2025-10-21 23:32:13 +0100

    create dto for candidates that passed an interview

---
+++ src/main/java/ma/nttdata/externals/module/candidate/dto/OfferPassedCandidatesDTO.java
@@ -0,0 +1,14 @@
+package ma.nttdata.externals.module.candidate.dto;
+
+
+import ma.nttdata.externals.module.interview.dto.FullEvaluationDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public record OfferPassedCandidatesDTO(
+        UUID id,
+        String fullName,
+        List<FullEvaluationDTO> evaluations
+){}
+
